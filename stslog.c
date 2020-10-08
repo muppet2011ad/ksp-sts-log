803691cc1287b4b1d5892215f10471323787da22
James Townsend <j.a.townsend2000@gmail.com>
2020-10-08 16:52:43 +0100

    Dynamically allocated orbiter array

--- stslog.c
+++ stslog.c
@@ -6,10 +6,32 @@
 #include "kerbal.h"
 #include "mission.h"
 
+void addOrbiter(orbiter *orbiters[], orbiter new_orbiter, int *next_free, int *max_size) { // Adds a new orbiter to a dynamically-allocated array of orbiters. First two params are hopefully obvious. Third is the index of the next available space. Fourth is the current size of the array.
+    if (*next_free >= *max_size) { // If we need to extend the array
+        orbiter *tempalloc = realloc(*orbiters, sizeof(orbiter)*(*max_size+5)); // Realloc memory to extend the array
+        if (tempalloc != NULL) { // If the realloc was successful
+            tempalloc[*next_free] = new_orbiter; // Add the new orbiter to the array
+            *next_free = *next_free + 1;
+            *max_size = *max_size + 5; // Update all the counters accordingly
+            *orbiters = tempalloc; // Replace the original orbiters pointer
+        }
+        else { // If the realloc was not successful
+            printf("\nError: failed to allocate new memory.\n"); // Output that there was a problem
+            return; // Get out of the function
+        }
+    }
+    else { // If we don't need to do anything memory-wise
+        *orbiters[*next_free] = new_orbiter; // Just add on the new orbiter
+        *next_free = *next_free + 1; // Increment the relevant counter
+    }
+}
+
 int main () {
-    orbiter orbiters[16];
-    orbiters[0] = initOrbiter("Enterprise");
-    orbiters[1] = initOrbiter("Columbia");
+    orbiter *orbiters = (orbiter *) calloc(5, sizeof(orbiter));
+    int next_free_orbiter = 0;
+    int max_size = 1;
+    addOrbiter(&orbiters, initOrbiter("Enterprise"), &next_free_orbiter, &max_size);
+    addOrbiter(&orbiters, initOrbiter("Columbia"), &next_free_orbiter, &max_size);
     
     kerbal kerbals[8];
     kerbals[0] = initKerbal("Jeb");
